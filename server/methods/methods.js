e56645ab2be41f1cc479970cc74bbd43d30b6bb4
HL <nunavutaccountant2@gmail.com>
2016-04-18 13:39:12 +0800

    Adding user method can now specify username and pwd

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -1,16 +1,39 @@
-import {Meteor} from 'meteor/meteor';
-import {Things} from '/lib/collections';
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Things } from '/lib/collections';
 
 export default function () {
   Meteor.methods({
     'wipeAndInitialize'() {
       Things.remove({});
 
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 3; i++) {
         Things.insert({
           text: `some text ${i}`
         });
       }
+    }
+  });
+
+  Meteor.methods({
+    'things.add'() {
+      const count = Things.find().count();
+
+      const id = Things.insert({
+        text: `some text ${count}`
+      });
+
+      return id;
+    }
+  });
+
+  Meteor.methods({
+    'users.add'({username, password}) {
+      const id = Accounts.createUser({
+        username,
+        password
+      });
+      return id;
     }
   });
 }
