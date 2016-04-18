122a0348352eb94f14ceed2f188c672d284ae396
HL <nunavutaccountant2@gmail.com>
2016-04-18 18:36:41 +0800

    Fixed bugs. Added inits

--- server/main.js
+++ server/main.js
@@ -1,3 +1,21 @@
 import methods from './methods';
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Things } from '/lib/collections';
 
 methods();
+
+const numUsers = Meteor.users.find().count();
+if (numUsers === 0) {
+  Accounts.createUser({
+    username: 'test',
+    password: '1'
+  });
+}
+
+const numThings = Things.find().count();
+if (numThings === 0) {
+  Things.insert({
+    text: `this is thing ${numThings}`
+  });
+}
